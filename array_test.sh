0db7520716b704d4b5d70affc296319b627d203c
James Wolfe <wolfe.work04@gmail.com>
2022-10-16 00:51:05 -0400

    lower portion of code works

--- array_test.sh
+++ array_test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+x=0
 t=0
 y=0
 index=''
@@ -7,25 +8,29 @@ lower=(a b c d e f g h i j k l m n o p q r s t u v w x y z)
 upper=(ABCDEFGHIJKLMNOPQRSTUVWXYZ)
 numbers=(1234567890)
 bot=0
-password='o'
+password='car'
 counter=0
 answer=''
 final=''
 
 while :
 do
-    if [ $final != $password ]; then
-        until [ $x == ${password[$t]} ]; do
+    if [ "$final" != $password ]; then
+        until [ $x == ${password:$t:1} ]; do
             for x in ${lower[$y]}; do
                 echo $x
-                ((y++))
+                ((y++)); ((counter++))
+                echo $counter
             done
         done
         final+=${x}
-    else
-        break
-    fi
+        ((h++))
+        ((t++))
+        y=0
+        else
+            break
+    fi 
 done
 
 echo ''
-echo $final
+echo $final $counter
